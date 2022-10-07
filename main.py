9940dae4aa7eb87de0eaf412a0dcba80c84b6ee2
StivenTA <92292686+StivenTA@users.noreply.github.com>
2022-10-07 14:08:29 +0700

    Baru

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import statistics
-
+import string
 
 def TF(wordDict, bagOfWords):
     tfDict = {}
@@ -46,16 +46,19 @@ def accuracy(y_true, y_pred):
 
 
 
-A = "The rain is pouring the"
-B = "The rain has stopped"
-C = "The sun is up The sun is set The sun is round Rain and sun are the best"
+A = 'The rain is pouring the'
+B = 'The rain has stopped'
+C = 'The sun is up The sun is set The sun is round Rain and sun are the best'
 Dataset = {
-    'data': ['Why sun is up at east and set at west?',
-             'There is only one sun and one moon at earth',
+    'data': ['Saya siapa?',
+             'siapa dosen yang akan mengajar pada semester ini?',
              'i love sunny day',
              'why people always skeptical when see sun is up at west, thats what i thought',
-             'Man this life is suck'],
-    'intent': [1, 2, 2, 1, 2]
+             'Man this life is suck',
+             'The sun is up The sun is set The sun is round Rain and sun are the best',
+             'The rain has stopped',
+             'The rain is pouring the'],
+    'intent': [1, 2, 2, 1, 2, 2, 2, 2]
     # 1 is Asking
     # 2 is Statement
 }
@@ -64,10 +67,12 @@ A = A.lower()
 bowDataset = {'data': []}
 
 for sentence in Dataset['data']:
-    bowDataset['data'].append(sentence.lower().split(' '))
-
+    # .translate(str.maketrans('','',string.punctuation)) digunakan untuk menghilangkan tanda baca
+    bowDataset['data'].append(sentence.translate(str.maketrans('','',string.punctuation)).lower().split(' '))
+print(bowDataset)
 uniqueWords = set()
 for word in bowDataset['data']:
+    #untuk mencari semua kata pada dataset
     uniqueWords = uniqueWords.union(set(word))
 
 
@@ -75,27 +80,35 @@ numOfWordsDataset = dict.fromkeys(uniqueWords, 0)
 
 for sentence in bowDataset['data']:
     for word in sentence:
+        #mencari jumlah setiap kata muncul dari seluruh dataset
         numOfWordsDataset[word] += 1
 
 tfDataset = {'data': []}
 for sentence in bowDataset['data']:
     numOfSentenceDataset = dict.fromkeys(sentence,0)
     for word in sentence:
+        #mencari jumlah kata muncul dari sebuah kalimat
         numOfSentenceDataset[word] += 1
+    #Menghitung nilai Term Frequency untuk setiap kalimat
     tfTemp = TF(numOfSentenceDataset,sentence)
     tfDataset['data'].append(tfTemp)
+    # print(numOfSentenceDataset)
 idfs = {'data': []}
 tfidf = {'data': []}
-for sentence in tfDataset['data']:
-    idfsTemp = IDF([numOfWordsDataset,sentence],len(Dataset['data']))
-    idfs['data'].append(idfsTemp)
 
+for sentence in tfDataset['data']:
+    # mencari nilai IDF pada setiap kalimat dataset
+    # len diambil untuk menghitung total dari kalimat yang ada
+    # IDF menggunakan numOfWordsDataset karena menghitung setiap kata pada kalimat pada seluruh kata unik yang ada
+    idfsTemp = IDF([numOfWordsDataset,sentence],len(Dataset['data']))
     tfidfTemp = TFIDF(sentence,idfsTemp)
     tfidf['data'].append(tfidfTemp)
 
 value = {'data': []}
 for sentence in tfidf['data']:
-    value['data'].append(sum(sentence.values()))
+    # nilai tfidf diubah menjadi nilai rata-rata
+    value['data'].append(statistics.mean(sentence.values()))
+print(value)
 
 
 X, y = value['data'], Dataset['intent']
@@ -109,4 +122,3 @@ clf = KNN(k=k)
 clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 print("KNN classification accuracy", accuracy(y_test, predictions))
-
